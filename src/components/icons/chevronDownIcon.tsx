2b922ce848ae35072224ad2ceb622732d7241db6
GCobo <gcobo92@gmail.com>
2020-11-20 09:34:36 +0100

    reduce chevron icon

--- src/components/icons/chevronDownIcon.tsx
+++ src/components/icons/chevronDownIcon.tsx
@@ -6,7 +6,7 @@ type IProps = {
 
 const ChevronDownIcon: React.FunctionComponent<IProps> = ({ className }) => {
   return (
-    <svg width='16' height='16' viewBox='0 0 24 24' className={className}>
+    <svg width='12' height='12' viewBox='0 0 24 24' className={className}>
       <path d='M12 19.413l12-12-2.821-2.825-9.179 9.185-9.177-9.185-2.823 2.823 12 12.002z'></path>
     </svg>
   )
