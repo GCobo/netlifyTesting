e20ff6b6844859cdd00597b8ba95f9fb5c83134b
Pedro José Peña <malagactiva@gmail.com>
2020-12-12 22:08:42 +0100

    fix(components): #102 missing properties onChange and Value in CheckboxPlatform

---
+++ src/components/checkPlatform/CheckPlatform.spec.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { mount } from 'cypress-react-unit-test';
+import { CheckPlatform, ICheckPlatform } from './CheckPlatform';
+import { JoinbleThemeProvider } from '../../providers';
+import { AndroidIcon, AppleIcon } from '../icons';
+
+const options: ICheckPlatform[] = [
+  {
+    id: '121221',
+    icon: <AndroidIcon />,
+    name: 'Android'
+  },
+  {
+    id: '121222',
+    icon: <AppleIcon />,
+    name: 'Apple'
+  }
+];
+
+describe('Input Upload component', () => {
+  it('should check checkbox android', () => {
+    mount(
+      <JoinbleThemeProvider>
+        <CheckPlatform options={options} name='platforms' onChange={() => {}} />
+      </JoinbleThemeProvider>
+    );
+
+    cy.get('[data-test="check-Android"]').click();
+
+    cy.get(`input[id="${options[0].id}"]`).should(
+      'have.attr',
+      'aria-checked',
+      'true'
+    );
+  });
+
+  it('should check both elements', () => {
+    mount(
+      <JoinbleThemeProvider>
+        <CheckPlatform options={options} name='platforms' onChange={() => {}} />
+      </JoinbleThemeProvider>
+    );
+
+    cy.get('[data-test="check-Android"]').click();
+    cy.get('[data-test="check-Apple"]').click();
+
+    cy.get(`input[id="${options[0].id}"]`).should(
+      'have.attr',
+      'aria-checked',
+      'true'
+    );
+
+    cy.get(`input[id="${options[1].id}"]`).should(
+      'have.attr',
+      'aria-checked',
+      'true'
+    );
+  });
+
+  it('should add one value by default', () => {
+    mount(
+      <JoinbleThemeProvider>
+        <CheckPlatform
+          options={options}
+          name='platforms'
+          value={['121222']}
+          onChange={() => {}}
+        />
+      </JoinbleThemeProvider>
+    );
+
+    cy.get(`input[id="${options[1].id}"]`).should(
+      'have.attr',
+      'aria-checked',
+      'true'
+    );
+  });
+});
--- src/components/checkPlatform/CheckPlatform.tsx
+++ src/components/checkPlatform/CheckPlatform.tsx
@@ -1,50 +1,75 @@
-import React, { ReactElement, useState } from 'react'
-import { ButtonIcon } from '../button'
+import React, { ReactElement, useEffect, useState } from 'react';
+import { ButtonIcon } from '../button';
 import {
   CheckPlatformInput,
   CheckPlatformOption,
   CheckPlatformWrapper
-} from './CheckPlatformStyles'
+} from './CheckPlatformStyles';
 
 export type ICheckPlatform = {
-  id: number
-  icon: ReactElement
-}
+  id: string;
+  icon: ReactElement;
+  name: string;
+};
 
 type IProps = {
-  className?: string
-  options: ICheckPlatform[]
-}
+  className?: string;
+  options: ICheckPlatform[];
+  name: string;
+  onChange(values: string[]): void;
+  value?: string[];
+};
 
-export const CheckPlatform = ({ className, options }: IProps) => {
-  const [checkPlatform, setCheckPlatform] = useState<number[]>([])
+export const CheckPlatform = ({
+  className,
+  options,
+  onChange,
+  name,
+  value = []
+}: IProps) => {
+  const [checkPlatform, setCheckPlatform] = useState<string[]>([]);
 
-  const handleSetCheckPlatform = (id: number) => {
+  const handleSetCheckPlatform = (id: string) => {
     setCheckPlatform((checkPlatform) =>
       checkPlatform.includes(id)
         ? checkPlatform.filter((key) => key !== id)
         : [...checkPlatform, id]
-    )
-  }
+    );
+  };
+
+  useEffect(() => {
+    if (value.length) {
+      setCheckPlatform(value);
+    }
+  }, [value]);
+
+  useEffect(() => {
+    if (checkPlatform) {
+      onChange && onChange(checkPlatform);
+    }
+  }, [checkPlatform]);
 
   return (
     <CheckPlatformWrapper className={className}>
-      {options.map((option: any) => {
+      {options.map((option: ICheckPlatform) => {
         return (
           <CheckPlatformOption key={option.id}>
             <CheckPlatformInput
               type='checkbox'
               checked={checkPlatform.includes(option.id)}
               id={option.id}
+              name={name}
+              aria-checked={checkPlatform.includes(option.id)}
             />
             <ButtonIcon
               secondary
               icon={option.icon}
               onClick={() => handleSetCheckPlatform(option.id)}
+              testId={`check-${option.name}`}
             />
           </CheckPlatformOption>
-        )
+        );
       })}
     </CheckPlatformWrapper>
-  )
-}
+  );
+};
