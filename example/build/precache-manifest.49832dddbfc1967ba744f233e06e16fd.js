d93e81613d19c5f791fde271b950c6505286a887
GCobo <gcobo92@gmail.com>
2021-05-26 00:15:02 +0200

    fix netlify build

---
+++ example/build/precache-manifest.49832dddbfc1967ba744f233e06e16fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4b631d0860cecbecb98792d94e6ae0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "284b3ce84602609d1d61",
+    "url": "./static/js/2.504d5988.chunk.js"
+  },
+  {
+    "revision": "d2568f64a67958025e0d5a6aceb710b4",
+    "url": "./static/js/2.504d5988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e688fb334ba94a5457d2",
+    "url": "./static/js/main.82e39f53.chunk.js"
+  },
+  {
+    "revision": "d52dfa14faf3c78d28dc",
+    "url": "./static/js/runtime-main.a28fdba6.js"
+  }
+]);
