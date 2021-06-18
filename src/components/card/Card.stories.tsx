62336f0906bbded13ccfa4e6a3422fe841dd4d60
GCobo <gcobo92@gmail.com>
2021-06-18 18:48:38 +0200

    fix card storybook and add buttonsocialmedia

--- src/components/card/Card.stories.tsx
+++ src/components/card/Card.stories.tsx
@@ -1,55 +1,59 @@
 import React from 'react';
 
-import styled from '@emotion/styled';
-
 import { ButtonIcon } from '../buttonIcon/ButtonIcon';
 import { SearchIcon } from '../icons';
 import { Card } from './Card';
 
 export default {
   title: 'Components/Card',
-  component: Card
+  component: Card,
+  argTypes: {}
 };
 
-const Wrapper = styled.div`
-  width: 200px;
-  height: 300px;
-`;
-
-export const CardBase = () => {
-  <Wrapper>
+export const CardBase = (args: any) => {
+  return (
     <Card
+      {...args}
       lastUpdate='25/7/2001'
       title='card '
       actions={<ButtonIcon icon={<SearchIcon />} />}
     />
-  </Wrapper>;
+  );
 };
 
-export const CardImage = () => {
-  <Card
-    lastUpdate='25/7/2001'
-    image='https://miro.medium.com/max/3600/1*HSisLuifMO6KbLfPOKtLow.jpeg'
-    title='card image'
-    actions={<ButtonIcon icon={<SearchIcon />} />}
-  />;
+export const CardImage = (args: any) => {
+  return (
+    <Card
+      {...args}
+      lastUpdate='25/7/2001'
+      image='https://miro.medium.com/max/3600/1*HSisLuifMO6KbLfPOKtLow.jpeg'
+      title='card image'
+      actions={<ButtonIcon icon={<SearchIcon />} />}
+    />
+  );
 };
 
-export const CardBigImage = () => {
-  <Card
-    imageBig
-    lastUpdate='25/7/2001'
-    image='https://miro.medium.com/max/3600/1*HSisLuifMO6KbLfPOKtLow.jpeg'
-    title='card image'
-    actions={<ButtonIcon icon={<SearchIcon />} />}
-  />;
+export const CardBigImage = (args: any) => {
+  return (
+    <Card
+      {...args}
+      imageBig
+      lastUpdate='25/7/2001'
+      image='https://miro.medium.com/max/3600/1*HSisLuifMO6KbLfPOKtLow.jpeg'
+      title='card image'
+      actions={<ButtonIcon icon={<SearchIcon />} />}
+    />
+  );
 };
 
-export const CardHorizontal = () => {
-  <Card
-    horizontal
-    lastUpdate='25/7/2001'
-    title='card horizontal'
-    actions={<ButtonIcon icon={<SearchIcon />} />}
-  />;
+export const CardHorizontal = (args: any) => {
+  return (
+    <Card
+      {...args}
+      horizontal
+      lastUpdate='25/7/2001'
+      title='card horizontal'
+      actions={<ButtonIcon icon={<SearchIcon />} />}
+    />
+  );
 };
